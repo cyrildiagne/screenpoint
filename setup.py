8c76f34c0324d25c3a18320a76dc605178aa8861
Cyril Diagne <diagne.cyril@gmail.com>
2020-05-03 08:25:42 +0200

    update version num

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 setup(
     name='screenpoint',
     packages=['screenpoint'],
-    version='0.1.1',
+    version='0.1.2',
     license='MIT',
     description='Project an image centroid to another image using OpenCV',
     long_description=long_description,
